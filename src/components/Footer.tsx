f3360d8003286db6d262fc188288c4d70862a75f
lucjastozek <lucjastozek@gmail.com>
2023-09-19 20:32:12 +0200

    clean up Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,25 +8,23 @@ const repoLinkStyle = {
 export function Footer() {
     return (
         <Center h="20vh">
-            <div style={{ textAlign: "center" }}>
-                <Text fontSize="xl" mt="5">
-                    You can visit our{" "}
-                    <a
-                        href="https://github.com/lucjastozek/study-resource-catalog-front"
-                        style={repoLinkStyle}
-                    >
-                        frontend
-                    </a>{" "}
-                    and{" "}
-                    <a
-                        href="https://github.com/danivoro/C7C2-study-app-server"
-                        style={repoLinkStyle}
-                    >
-                        backend
-                    </a>{" "}
-                    repositories here.
-                </Text>
-            </div>
+            <Text fontSize="xl" mt="5" textAlign={"center"}>
+                You can visit our{" "}
+                <a
+                    href="https://github.com/lucjastozek/study-resource-catalog-front"
+                    style={repoLinkStyle}
+                >
+                    frontend
+                </a>{" "}
+                and{" "}
+                <a
+                    href="https://github.com/danivoro/C7C2-study-app-server"
+                    style={repoLinkStyle}
+                >
+                    backend
+                </a>{" "}
+                repositories here.
+            </Text>
         </Center>
     );
 }
