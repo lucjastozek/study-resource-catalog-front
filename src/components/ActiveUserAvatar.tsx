0121b3157bb032b986ec406167796f34e495be90
TomOwen93 <mr.tsowen@gmail.com>
2023-09-19 22:25:58 +0100

    Clean up Header.tsx, add ActiveUserAvatar.tsx

---
+++ src/components/ActiveUserAvatar.tsx
@@ -0,0 +1,101 @@
+import {
+    Avatar,
+    AvatarBadge,
+    Box,
+    Button,
+    Popover,
+    PopoverArrow,
+    PopoverBody,
+    PopoverCloseButton,
+    PopoverContent,
+    PopoverHeader,
+    PopoverTrigger,
+    Portal,
+    Text,
+} from "@chakra-ui/react";
+
+import { initialUser } from "../utils/initialUser";
+import { User } from "../interface/User";
+
+interface ActiveUserAvatarProps {
+    activeUser: User;
+    userImage: string;
+    setActiveUser: React.Dispatch<React.SetStateAction<User>>;
+}
+
+export function ActiveUserAvatar({
+    userImage,
+    activeUser,
+    setActiveUser,
+}: ActiveUserAvatarProps): JSX.Element {
+    return (
+        <>
+            <Popover>
+                <Box gridArea={"avatar"} width={"20vw"} marginLeft={"auto"}>
+                    <PopoverTrigger>
+                        <Box textAlign="center" mb={"4"}>
+                            <Avatar src={userImage} name={activeUser?.name}>
+                                {activeUser.user_id > 0 && (
+                                    <AvatarBadge
+                                        boxSize="1.25em"
+                                        bg="green.500"
+                                    />
+                                )}
+                            </Avatar>
+
+                            {userImage !== "./undefined-avatar.png" && (
+                                <Text mt={2}>{activeUser?.name}</Text>
+                            )}
+                            {userImage === "./undefined-avatar.png" && (
+                                <Text mt={2}>
+                                    Login to submit {activeUser?.name}{" "}
+                                </Text>
+                            )}
+                        </Box>
+                    </PopoverTrigger>
+                </Box>
+
+                <Portal>
+                    {userImage !== "./undefined-avatar.png" && (
+                        <PopoverContent>
+                            <PopoverArrow />
+                            <PopoverHeader>
+                                Do you want to sign out?
+                            </PopoverHeader>
+                            <PopoverCloseButton />
+                            <PopoverBody>
+                                <Button
+                                    colorScheme="blue"
+                                    onClick={() =>
+                                        setActiveUser(JSON.parse(initialUser))
+                                    }
+                                >
+                                    <a href="/users">Sign Out</a>
+                                </Button>
+                            </PopoverBody>
+                        </PopoverContent>
+                    )}
+                    {userImage === "./undefined-avatar.png" && (
+                        <PopoverContent>
+                            <PopoverArrow />
+                            <PopoverHeader>
+                                Do you want to sign in?
+                            </PopoverHeader>
+                            <PopoverCloseButton />
+                            <PopoverBody>
+                                <Button
+                                    colorScheme="blue"
+                                    onClick={() =>
+                                        setActiveUser(JSON.parse(initialUser))
+                                    }
+                                >
+                                    <a href="/users">Sign in</a>
+                                </Button>
+                            </PopoverBody>
+                        </PopoverContent>
+                    )}
+                </Portal>
+            </Popover>
+        </>
+    );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,6 @@
-import {
-    Avatar,
-    AvatarBadge,
-    Box,
-    Button,
-    Grid,
-    Heading,
-    Popover,
-    PopoverArrow,
-    PopoverBody,
-    PopoverCloseButton,
-    PopoverContent,
-    PopoverHeader,
-    PopoverTrigger,
-    Portal,
-    Text,
-    useMediaQuery,
-} from "@chakra-ui/react";
+import { Grid, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 import { User } from "../interface/User";
-import { initialUser } from "../utils/initialUser";
+import { ActiveUserAvatar } from "./ActiveUserAvatar";
 
 interface HeaderProps {
     userImage: string;
@@ -57,76 +40,11 @@ export function Header({
                     Study Resources Catalog
                 </Heading>
 
-                <Popover>
-                    <Box gridArea={"avatar"} width={"20vw"} marginLeft={"auto"}>
-                        <PopoverTrigger>
-                            <Box textAlign="center" mb={"4"}>
-                                <Avatar src={userImage} name={activeUser?.name}>
-                                    {activeUser.user_id > 0 && (
-                                        <AvatarBadge
-                                            boxSize="1.25em"
-                                            bg="green.500"
-                                        />
-                                    )}
-                                </Avatar>
-
-                                {userImage !== "./undefined-avatar.png" && (
-                                    <Text mt={2}>{activeUser?.name}</Text>
-                                )}
-                                {userImage === "./undefined-avatar.png" && (
-                                    <Text mt={2}>
-                                        Login to submit {activeUser?.name}{" "}
-                                    </Text>
-                                )}
-                            </Box>
-                        </PopoverTrigger>
-                    </Box>
-
-                    <Portal>
-                        {userImage !== "./undefined-avatar.png" && (
-                            <PopoverContent>
-                                <PopoverArrow />
-                                <PopoverHeader>
-                                    Do you want to sign out?
-                                </PopoverHeader>
-                                <PopoverCloseButton />
-                                <PopoverBody>
-                                    <Button
-                                        colorScheme="blue"
-                                        onClick={() =>
-                                            setActiveUser(
-                                                JSON.parse(initialUser)
-                                            )
-                                        }
-                                    >
-                                        <a href="/users">Sign Out</a>
-                                    </Button>
-                                </PopoverBody>
-                            </PopoverContent>
-                        )}
-                        {userImage === "./undefined-avatar.png" && (
-                            <PopoverContent>
-                                <PopoverArrow />
-                                <PopoverHeader>
-                                    Do you want to sign in?
-                                </PopoverHeader>
-                                <PopoverCloseButton />
-                                <PopoverBody>
-                                    <Button
-                                        colorScheme="blue"
-                                        onClick={() =>
-                                            setActiveUser(
-                                                JSON.parse(initialUser)
-                                            )
-                                        }
-                                    >
-                                        <a href="/users">Sign in</a>
-                                    </Button>
-                                </PopoverBody>
-                            </PopoverContent>
-                        )}
-                    </Portal>
-                </Popover>
+                <ActiveUserAvatar
+                    activeUser={activeUser}
+                    setActiveUser={setActiveUser}
+                    userImage={userImage}
+                />
             </Grid>
         </header>
     );
